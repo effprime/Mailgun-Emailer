7f0052f3347bac5ad4b04a05d4829e48707c0226
effprime <effprime@github.com>
2019-11-16 13:09:25 -0800

    Create MailgunEmailer class.

--- mail.py
+++ mail.py
@@ -1,28 +1,67 @@
-from requests import post as http_post
-from json import load as json_load
-from datetime import datetime as right
+"""Wrapper for sending emails using the Mailgun API
+"""
+import requests
+import yaml
 
-def send_login_alert(
-    api_url, 
-    api_key, 
-    from_addr, 
-    to_addr,
-    subject,
-    message,
-    time
-):
-	return http_post(
-		api_url,
-		auth=("api", api_key),
-		data={
-            "from": from_addr, 
-            "to": to_addr, 
-            "subject": subject,
-            "text": message + "\n\n" + "Time: %s" % (time)
-        }
-    )
+class MailgunEmailer:
+    """The Mailgun Emailer object
+    """
 
-with open("data.json","r") as file:
-    args = json_load(file)
-args["time"] = right.now()
-send_login_alert(**args)
+    def __init__(self, api_url=False, api_key=False):
+        """Creates an emailer for a Mailgun account.
+
+        args:
+            api_url (str): the Mailgun API endpoint
+            api_key (str): the Mailgun account API key
+        """
+        if not api_key or not api_url:
+            self.yaml = self.load_yaml("data")
+        else:
+            self.yaml = None
+        try:
+            if not api_key:
+                api_key = self.yaml["api_key"]
+            if not api_url:
+                api_url = self.yaml["api_url"]
+        except KeyError as error:
+            raise f"Yaml value not found: {error}"
+        self.api_url = api_url
+        self.api_key = api_key
+
+    @staticmethod
+    def load_yaml(file_name):
+        """Loads the contents of a yaml file.
+
+        args:
+            file_name (str): the yaml filename (without extension)
+        """
+        try:
+            with open(f"{file_name}.yaml", "r") as yaml_file:
+                return yaml.load(yaml_file)
+        except FileNotFoundError:
+            raise f"No `{file_name}.yaml` file found"
+
+    def send_from_args(self, **kwargs):
+        """Sends an email using the Mailgun email parameters.
+
+        args:
+            kwargs (dict): the email parameters (`from`, `to`, `subject`, `text`)
+        """
+        try:
+            data = {
+                "from": kwargs["from_addr"],
+                "to": kwargs["to_addr"],
+                "subject": kwargs["subject"],
+                "text": kwargs["message"]
+            }
+        except KeyError as error:
+            raise f"Yaml value not found: {error}"
+        return requests.post(self.api_url, auth=("api", self.api_key), data=data)
+
+    def send_from_yaml(self, file_name):
+        """Sends an email using parameters in a yaml file.
+
+        args:
+            file_name (str): the yaml filename (without extension)
+        """
+        return self.send_from_args(**self.load_yaml(file_name))
